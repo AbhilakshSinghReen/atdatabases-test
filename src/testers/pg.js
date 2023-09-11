657cec4ffaaadb1f3a63b1736c08fe91621e24c4
AbhilakshSinghReen <abhilakshreen@gmail.com>
2023-09-11 16:10:01 +0530

    Printing users to console before inserting them.

--- src/testers/pg.js
+++ src/testers/pg.js
@@ -26,16 +26,34 @@ async function testPg(connectionPoolConfig) {
     INSERT INTO users (email, foo_date, foo_timestamp)
         VALUES ('me1@example.com', '2023-09-02', '2023-09-02 12:00:00')
   `);
+  console.log("Inserting user 1:- ");
+  console.log({
+    email: "me1@example.com",
+    foo_date: "2023-09-02",
+    foo_timestamp: "2023-09-02 12:00:00",
+  });
 
   result = await db.query(sql`
     INSERT INTO users (email, foo_date, foo_timestamp)
         VALUES ('me2@example.com', '2023-09-04', '2023-09-04 12:00:00')
   `);
+  console.log("Inserting user 2:- ");
+  console.log({
+    email: "me2@example.com",
+    foo_date: "2023-09-04",
+    foo_timestamp: "2023-09-04 12:00:00",
+  });
 
   result = await db.query(sql`
     INSERT INTO users (email, foo_date, foo_timestamp)
         VALUES ('me3@example.com', '2023-09-06', '2023-09-06 12:00:00')
   `);
+  console.log("Inserting user 3:- ");
+  console.log({
+    email: "me3@example.com",
+    foo_date: "2023-09-06",
+    foo_timestamp: "2023-09-06 12:00:00",
+  });
   console.log("3 users inserted");
 
   console.log("Feteching all users...");
